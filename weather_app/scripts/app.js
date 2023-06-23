ce46b49c9aba75e1e1b70349a89b2b4d1c366989
william5 <samwilliamj333@gmail.com>
2023-06-23 21:56:30 +0530

    js projects

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -27,8 +27,51 @@ form.addEventListener('submit', (e)=>{
     const inputCity = form.city.value.trim();
     form.reset();
     
+    
+    
    updateCity(inputCity)
    .then(data => Dynamic(data))
    .catch(err => console.error(err));
+
+   localStorage.setItem('city', inputCity);
+
  });
+
+ if(localStorage.getItem('city')){
+  updateCity(localStorage.getItem('city'))
+  .then(data => Dynamic(data))
+  .catch(err => console.error(err));
+
+ }
+
+ console.log(window.localStorage);
+ console.log(window);
+
+
+const station =  [
+   {
+     color: 'red',
+      type: 'station wagon',
+       registration: 'Sat Mar 03 2018 01:00:00 GMT+0100 (GMT+01:00)',
+       capacity: 5
+     },
+     {
+       color: 'red',
+       type: 'cabrio',
+       registration: 'Sat Mar 03 2012 01:00:00 GMT+0100 (GMT+01:00)',
+       capacity: 2
+     }
+   ];
+
+let age = localStorage.setItem('station',JSON.stringify(station));
+
+console.log(localStorage.getItem('station'));
+
+
+
+
+
+
+
+
 
