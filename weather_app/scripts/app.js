5f13e6e5c18d19586f86452c8bfe29132cdd9219
william5 <samwilliamj333@gmail.com>
2023-06-28 22:20:45 +0530

    js projects

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -3,12 +3,15 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const cloudyIcon = document.querySelector('.icon img');
-const updateCity = async (inputCity) =>{
-    const cityDetails = await fetched(inputCity);
-    const weathers = await weather(cityDetails.Key);
-    return {cityDetails , weathers };
 
-};
+const forecast = new Forecast();
+
+// const updateCity = async (inputCity) =>{
+//     const cityDetails = await fetched(inputCity);
+//     const weathers = await weather(cityDetails.Key);
+//     return {cityDetails , weathers };
+
+// };
 
 const Dynamic = (data) =>{
     const {cityDetails ,weathers} = data;
@@ -29,7 +32,7 @@ form.addEventListener('submit', (e)=>{
     
     
     
-   updateCity(inputCity)
+   forecast.updateCity(inputCity)
    .then(data => Dynamic(data))
    .catch(err => console.error(err));
 
@@ -38,7 +41,7 @@ form.addEventListener('submit', (e)=>{
  });
 
  if(localStorage.getItem('city')){
-  updateCity(localStorage.getItem('city'))
+  forecast.updateCity(localStorage.getItem('city'))
   .then(data => Dynamic(data))
   .catch(err => console.error(err));
 
@@ -66,6 +69,57 @@ const station =  [
 let age = localStorage.setItem('station',JSON.stringify(station));
 
 console.log(localStorage.getItem('station'));
+
+
+class persons{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+        this.score = 0;
+    }
+    login(){
+      console.log(`${this.name} is logged in`);
+      return this;
+    }
+    logout(){
+      console.log(`${this.name} is logged out`);
+      return this;
+    }
+    increment(){
+      this.score +=1;
+      console.log(`${this.name} is scored ${this.score}`);
+      return this;
+
+    }
+}
+
+
+class admin extends persons{
+  constructor(name,age,title){
+      super(name,age)
+      {
+        this.title = title;
+      }
+      }
+      deletePerson(p){
+        groupOfPersons = groupOfPersons.filter(g => g.name !== p.name);
+       }
+  }
+  
+
+
+
+const personOne = new persons('sam',26);
+const personTwo = new persons('william',30);
+let groupOfPersons = [personOne, personTwo];
+const admins = new admin('bob',23,'bob_store');
+console.log(personOne.login().logout().increment());
+console.log(personTwo.login().logout().increment());
+
+admins.deletePerson(personTwo);
+
+console.log(admins);
+console.log(groupOfPersons);
 
 
 
--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -1,28 +1,57 @@
-const api = 'AcdNdx5Y1XMlXhFqT7Yasp3b99rKP5h7';
+// const api = 'AcdNdx5Y1XMlXhFqT7Yasp3b99rKP5h7';
+class Forecast{
+    constructor(){
+        this.key = 'AcdNdx5Y1XMlXhFqT7Yasp3b99rKP5h7';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/'; 
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city){
+        const cityDetails = await this.getCity(city);
+        const weathers = await this.getWeather(cityDetails.Key);
+        return {cityDetails , weathers };
+    }
+    async getCity(city){
+        const query = `?apikey=${this.key}&q=${city}`;
 
-const fetched = async (city) =>{
-   const url = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-   const query = `?apikey=${api}&q=${city}`;
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
+        console.log(data[0]);
+     
+        return data[0];
+    }
+    async getWeather(id){
+        const query =`${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+        console.log(data[0]);
+    
+        return data[0];
+    }
+}
 
-   const response = await fetch(url + query);
-   const data = await response.json();
-   console.log(data[0]);
+// const fetched = async (city) =>{
+//    const url = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+//    const query = `?apikey=${api}&q=${city}`;
 
-   return data[0];
-};
+//    const response = await fetch(url + query);
+//    const data = await response.json();
+//    console.log(data[0]);
 
-const weather = async (id) =>{
-    const url = `http://dataservice.accuweather.com/currentconditions/v1/`;
-    const query =`${id}?apikey=${api}`;
+//    return data[0];
+// };
 
-    const response = await fetch(url + query);
-    const data = await response.json();
-    console.log(data[0]);
+// const weather = async (id) =>{
+//     const url = `http://dataservice.accuweather.com/currentconditions/v1/`;
+//     const query =`${id}?apikey=${api}`;
 
-    return data[0];
-};
+//     const response = await fetch(url + query);
+//     const data = await response.json();
+//     console.log(data[0]);
+
+//     return data[0];
+// };
  
-fetched('chennai')
+getCity('chennai')
 .then(data => weather(data.Key))
 .then(data => console.log(data))
 .catch(err => console.error(err));
